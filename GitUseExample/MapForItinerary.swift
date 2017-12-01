0866ed761d86a278c48c1e9f5f9f6fada33baf11
Tigran <tigran@macbook-pro-tigran.local>
2017-12-01 14:58:35 +0300

    Adding new map_for_itinerary feature

---
+++ GitUseExample/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  GitUseExample
+//
+//  Created by Tigran on 01.12.2017.
+//  Copyright © 2017 Tigran. All rights reserved.
+//
+
+import UIKit
+
+class MapForItinerary: NSObject {
+
+}
